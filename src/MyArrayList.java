7cdb72315be915f5d135de8b9e81c4a83ff8eb6e
slavicf <jaroslavfrunt@gmail.com>
2018-02-24 10:29:07 +0200

    Задание 1 - ArrayList - rebuilt

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,26 +1,46 @@
+import java.util.Arrays;
+
 public class MyArrayList<T> {
 //    Написать свой класс MyArrayList как аналог классу ArrayList.
 //    Можно использовать 1 массив для хранения данных.
 
+    @SuppressWarnings("unchecked")
     private T[] arrayList = (T[]) new Object[0];
 
     public void add(T value) {                              // добавляет элемент в конец
-        T[] tempList = (T[]) new Object[arrayList.length + 1];
+//        @SuppressWarnings("unchecked")
+//        T[] tempList = (T[]) new Object[arrayList.length + 1];
 
-        for (int i = 0; i < arrayList.length; i++){
-            tempList[i] = arrayList[i];
-        }
-        tempList[tempList.length - 1] = value;
-        arrayList = tempList;
+//        for (int i = 0; i < arrayList.length; i++){
+//            tempList[i] = arrayList[i];
+//        }
+
+//        T[] tempList = Arrays.copyOf(arrayList, arrayList.length + 1);
+//        tempList[arrayList.length] = value;
+//        arrayList = tempList;
+        int arrSize = arrayList.length;
+        arrayList = Arrays.copyOf(arrayList, arrSize + 1);
+        arrayList[arrSize] = value;
     }
 
     public void remove(int index) {                         // удаляет элемент под индексом
-        T[] tempList = (T[]) new Object[arrayList.length - 1];
-        int i2 = 0;
-
-        for (int i = 0; i < tempList.length; i++){
-            if (i != index) tempList[i2++] = arrayList[i];
-        }
+//        @SuppressWarnings("unchecked")
+//        T[] tempList = (T[]) new Object[arrayList.length - 1];
+//        int i2 = 0;
+//
+//        for (int i = 0; i < tempList.length; i++) {
+//            if (i != index) tempList[i2++] = arrayList[i];
+//        }
+//        arrayList = tempList;
+        int arrSize = arrayList.length - 1;
+//        T[] tempList1 = Arrays.copyOfRange(arrayList, 0, index);
+//        T[] tempList2 = Arrays.copyOfRange(arrayList, index, arrSize - 2);
+//        tempList = Arrays.copyOfRange(arrayList, index, arrSize - 2);
+//        arrayList = tempList + tempList;
+        @SuppressWarnings("unchecked")
+        T[] tempList = (T[]) new Object[arrSize];
+        System.arraycopy(arrayList, 0, tempList, 0, index);
+        System.arraycopy(arrayList, index + 1, tempList, index, arrSize - index);
         arrayList = tempList;
     }
 
